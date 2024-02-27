b823ac406a88e5c72ea92601570a11526a8301ef
hassanhanif72 <hassanhanif72@gmail.com>
2024-02-28 03:53:07 +0500

    Upload Pages

---
+++ assets/js/main.js
@@ -0,0 +1,129 @@
+// Top Slick Slider
+
+$('.top-slider').slick({
+  dots: true,
+  infinite: true,
+  autoplay: true,
+  arrows:false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false
+      }
+    }
+
+  ]
+});
+
+// client Slick Slider
+
+$('.client-slider').slick({
+  dots: false,
+  infinite: true,
+  autoplay: true,
+  arrows:false,
+  autoplaySpeed: 0,
+  speed: 7000,  
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false
+      }
+    }
+
+  ]
+});
+
+// Get Quote Form validation
+
+(function() {
+  'use strict';
+  var forms = document.querySelectorAll('.needs-validation');
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function(form) {
+    form.addEventListener('submit', function(event) {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+
+      form.classList.add('was-validated');
+    }, false);
+  });
+})();
+
+
+$(document).ready(function() {
+  $('.nav-item.dropdown').hover(
+    function() {
+      $(this).addClass('show');
+      $(this).find('.dropdown-menu').first().addClass('show');
+    },
+    function() {
+      $(this).removeClass('show');
+      $(this).find('.dropdown-menu').first().removeClass('show');
+    }
+  );
+});
+
+// scrool header fixed
+const header = document.querySelector('.header');
+function toggleFixedHeader() {
+    if (window.scrollY > 100) { 
+        header.classList.add('fixed-header');
+    } else {
+        header.classList.remove('fixed-header');
+    }
+}
+window.addEventListener('scroll', toggleFixedHeader);
+toggleFixedHeader();
+
+// end 
+
+
